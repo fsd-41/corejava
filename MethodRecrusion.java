452e72adbb2089d2492774ce034aa544b4bf23c8
fsd-41 <sangeethamathi.uniq@gmail.com>
2024-06-08 13:14:42 +0530

    factorial by method recursion

--- MethodRecrusion.java
+++ MethodRecrusion.java
@@ -23,11 +23,16 @@ class FactRecursion{
 			return 1;
 		}
 		
-		return i*factEx(i-1);//5*factEx(4)
+		return i*factEx(i-1);/*   5*factEx(4)//5*24
+								  4*fact(3)//4*6
+                              3*fact(2)  //3*2 2*fact(1)//2*1							  
+		
+		
+		*/
 	}
 	public static void main(String[] args){
 		FactRecursion OBJ =new FactRecursion();
 		System.out.println(OBJ.factEx(5));//argument
-	}
+	}  
 	
 }
